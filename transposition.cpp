a5e2dee8482e34d5bfda550341f55ef91abad13e
MasterHope <francesco.bellezza00@gmail.com>
2024-10-02 21:17:29 +0200

    removed cleaning. table up to 8 mebibyte.

--- transposition.cpp
+++ transposition.cpp
@@ -4,23 +4,8 @@
 using namespace chess;
 //logging
 //#define LOGGING_TT
-//cleaning if it is required...
-//#define CLEANING
 void TranspositionTable::store(Board &board, TTEntry ttEntry){
     uint64_t index = this->hash(board);
-    #ifdef CLEANING
-        if (this->mustClean()){
-            #ifdef LOGGING_TT
-            std::clog<<"Transposition table cleaning process"<<std::endl;
-            std::clog<<"Number of elements before the process:"<<num_elements<<std::endl;        
-            #endif
-            
-            this->cleanUp(board.halfMoveClock());
-            #ifdef LOGGING_TT
-            std::clog<<"Number of elements after the process:"<<num_elements<<std::endl;
-            #endif
-        }
-    #endif
     if (this->hasCollisionAt(index)){
         this->replace(index, ttEntry);
     } else {
@@ -31,30 +16,6 @@ void TranspositionTable::store(Board &board, TTEntry ttEntry){
         }
     }
 };
-
-bool TranspositionTable::mustClean(){
-    //partial clean up when the table is occupied more than 90 percent.
-    return num_elements >= (int(TTSIZE*0.90));
-}
-
-void TranspositionTable::cleanUp(uint32_t clock){
-    int init_element = num_elements;
-    for (int i = 0; i < TTSIZE; i++){
-        TTEntry ttEntry = tt[i];
-        if (ttEntry.flag != EMPTY){
-            if (ttEntry.age < clock){
-                #pragma omp critical
-                {
-                    tt[i] = TTEntry();
-                    num_elements--;
-                }
-                if (num_elements <= init_element * 0.40){
-                    return;
-                }
-            }
-        }
-    }
-}
 
 TTEntry TranspositionTable::lookup(Board &board){
     int index = this->hash(board);
--- transposition.h
+++ transposition.h
@@ -5,7 +5,7 @@
 #include <iostream>
 using namespace chess;
 
-constexpr std::size_t TTSIZE = static_cast<std::size_t>(1) << 25;
+constexpr std::size_t TTSIZE = static_cast<std::size_t>(1) << 26;
 
 #define EMPTY -1
 #define EXACT 0
