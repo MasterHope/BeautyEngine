5180156341eb90f8e7098d9045e6d6c947ad8680
MasterHope <francesco.bellezza00@gmail.com>
2024-10-08 01:04:06 +0200

    2^22 bit of transposition table.

--- transposition.h
+++ transposition.h
@@ -6,7 +6,7 @@
 #include <mutex>
 using namespace chess;
 
-constexpr std::size_t TTSIZE = static_cast<std::size_t>(1) << 16;
+constexpr std::size_t TTSIZE = static_cast<std::size_t>(1) << 22;
 
 #define EMPTY -1
 #define EXACT 0
